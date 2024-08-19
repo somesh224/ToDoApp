09ff921704d42088a2980713a1a3e614acc76886
somesh224 <someshmishra224@gmail.com>
2024-08-19 23:55:41 +0530

    2nd commit

--- app/src/main/java/com/somesh/todoapp/RegisterActivity.kt
+++ app/src/main/java/com/somesh/todoapp/RegisterActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.android.volley.*
+import com.android.volley.toolbox.HttpHeaderParser
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.somesh.todoapp.UtilsService.UtilService
@@ -19,6 +21,7 @@ import org.json.JSONObject
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     private val utilService = UtilService()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -31,11 +34,15 @@ class RegisterActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        val name = binding.nameET
+        val email = binding.emailET
+        val password = binding.passwordET
+
         binding.registerBtn.setOnClickListener {
             utilService.closeKeyBoard(this)
-            binding.nameET.text.toString()
-            binding.emailET.text.toString()
-            binding.passwordET.text.toString()
+            name.text.toString()
+            email.text.toString()
+            password.text.toString()
             if (validate(view)) {
                 registerUser(view)
             }
@@ -49,36 +56,70 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun registerUser(view: View) {
+        val name = binding.nameET
+        val email = binding.emailET
+        val password = binding.passwordET
+
         binding.progressBar.visibility = View.VISIBLE
         val params: HashMap<String, String> = HashMap()
-        params["name"] = binding.nameET.text.toString()
-        params["email"] = binding.emailET.text.toString()
-        params["password"] = binding.passwordET.text.toString()
+        params["username"] = name.text.toString()
+        params["email"] = email.text.toString()
+        params["password"] = password.text.toString()
+
+        //converting above hashmap to json object
+        val jsonObject = JSONObject(params as Map<*, *>)
 
         val apiKey = "https://todoapi-zs2j.onrender.com/api/todo/auth/register"
-        val queue = Volley.newRequestQueue(this)
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, apiKey, null,
-            { response: JSONObject ->
 
+        //creating volley string request
+
+        val queue = Volley.newRequestQueue(this)
+
+        //creating a json object request
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, apiKey, jsonObject,
+            { response: JSONObject ->
+                try {
+                    if(response.getBoolean("success")) {
+                        val token = response.getString("token")
+                        Toast.makeText(this,token,Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    binding.progressBar.visibility = View.GONE
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                    binding.progressBar.visibility = View.GONE
+                }
             }, { error: VolleyError ->
                 val networkResponse: NetworkResponse? = error.networkResponse
                 if (error is ServerError && networkResponse != null) {
                     try {
-                        val res = String(networkResponse.data)
-                        val jsonObject = JSONObject(res)
-                        val message = jsonObject.getString("msg")
-                        utilService.showSnackBar(view, message)
+                        val res = networkResponse.data?.let { data ->
+                            val charset = HttpHeaderParser.parseCharset(networkResponse.headers, "utf-8")
+                            String(data, charset(charset))
+                        }
+                        if (res != null) {
+                            val jsonObject2 = JSONObject(res)
+                            val message = jsonObject2.getString("msg")
+                            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                        } else {
+                            // Handle the null case
+                            utilService.showSnackBar(view, "Error parsing response")
+                        }
+                        binding.progressBar.visibility = View.GONE
                     } catch (e: JSONException) {
                         e.printStackTrace()
+                        binding.progressBar.visibility = View.GONE
                     }
                 }
             }
         )
 
+        //setting headers
         fun Header(): MutableMap<String, String> {
             val headers = HashMap<String, String>()
             headers["Content-Type"] = "application/json"
-            return headers
+            return params
         }
 
         // set retry policy
@@ -94,6 +135,7 @@ class RegisterActivity : AppCompatActivity() {
         queue.add(jsonObjectRequest)
 
     }
+
 
     fun validate(view: View): Boolean {
         val isValid: Boolean
